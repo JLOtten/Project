11a2e33b07dda08bccdca96d4d6ef7fb7d9cfc0d
Jenna Otten <jenna.otten@gmail.com>
2022-10-26 12:13:43 -0700

    Updated encouragement to template

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 """Server for Coder's Boost app"""
 
 from flask import (Flask, render_template, request, flash, session, redirect)
-
+from model import connect_to_db, db
+import crud
 
 from jinja2 import StrictUndefined
 
@@ -9,11 +10,16 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+from model import Encouragement, db, connect_to_db
+
 @app.route("/")
 def homepage():
     """Home"""
 
-    return render_template("homepage.html")
+    encouragement = Encouragement.query.filter().first()
+    return render_template("homepage.html", encouragement=encouragement)
 
 if __name__ == "__main__":
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
+    
