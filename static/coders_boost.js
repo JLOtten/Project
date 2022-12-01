7bf5629074c97317f617befd868394561d792ba4
Jenna Otten <jenna.otten@gmail.com>
2022-11-30 20:59:12 -0800

    Fix div id to submit email and format javascript

--- static/coders_boost.js
+++ static/coders_boost.js
@@ -1,75 +1,75 @@
 function copyText() {
-    let element = document.getElementById("copyButton");
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(element.getAttribute("data-value"));
-  
-  }
+  let element = document.getElementById("copyButton");
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(element.getAttribute("data-value"));
 
-    //make an event listener for favorite button
-    function favoriteEncouragement() {
-      let element = document.getElementById("favoriteEncouragement");
+}
 
-      const encouragement_id = element.getAttribute("data-value");
+//make an event listener for favorite button
+function favoriteEncouragement() {
+  let element = document.getElementById("favoriteEncouragement");
 
-      fetch(`${window.location.protocol}//${window.location.host}/save-favorite`, { //this constructs the url
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', //tells server this is a json request
-        },
-        credentials: 'include',  //sends user credentials, so it knows which user is sending request
-        body: JSON.stringify({encouragement_id: encouragement_id}) //takes JS object and turns it into a string
-      })
-      .then(function () {   //anonymous function, not doing anything with response (vs code suggested this)
-        console.log("success saving encouragement");
-      })
-      .catch((error)=> {  // in the case there was some error
-        console.error('Error:', error); // log an error to the console
-      });
-    }
+  const encouragement_id = element.getAttribute("data-value");
 
-  //make an event listener for delete button
-  function deleteFavoriteEncouragement(encouragement_id) {
-    fetch(`${window.location.protocol}//${window.location.host}/delete-favorite`, { //this constructs the url
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', //tells server this is a json request
-      },
-      credentials: 'include',  //sends user credentials, so it knows which user is sending request
-      body: JSON.stringify({encouragement_id: encouragement_id}) //takes JS object and turns it into a string
+  fetch(`${window.location.protocol}//${window.location.host}/save-favorite`, { //this constructs the url
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', //tells server this is a json request
+    },
+    credentials: 'include',  //sends user credentials, so it knows which user is sending request
+    body: JSON.stringify({ encouragement_id: encouragement_id }) //takes JS object and turns it into a string
+  })
+    .then(function () {   //anonymous function, not doing anything with response (vs code suggested this)
+      console.log("success saving encouragement");
     })
+    .catch((error) => {  // in the case there was some error
+      console.error('Error:', error); // log an error to the console
+    });
+}
+
+//make an event listener for delete button
+function deleteFavoriteEncouragement(encouragement_id) {
+  fetch(`${window.location.protocol}//${window.location.host}/delete-favorite`, { //this constructs the url
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', //tells server this is a json request
+    },
+    credentials: 'include',  //sends user credentials, so it knows which user is sending request
+    body: JSON.stringify({ encouragement_id: encouragement_id }) //takes JS object and turns it into a string
+  })
     .then(function () {   //anonymous function, not doing anything with response (vs code suggested this)
       const element = document.getElementById("encouragement-" + encouragement_id); //grabs div id & encouragement_id
       element.remove(); // Removes deleted encouragement from page on successful submission
     })
-    .catch((error)=> {  // in the case there was some error
+    .catch((error) => {  // in the case there was some error
       console.error('Error:', error); // log an error to the console
     });
-  }
+}
 
-  //language selection dropdown button triggers translation of site to en or es
-  function selectLanguage(language) {
-    fetch(`${window.location.protocol}//${window.location.host}/language/${language}`, { //changes language using variable in route
-      method: 'GET',
-      credentials: 'include',  //sends user credentials, so it knows which user is sending request
-    })
+//language selection dropdown button triggers translation of site to en or es
+function selectLanguage(language) {
+  fetch(`${window.location.protocol}//${window.location.host}/language/${language}`, { //changes language using variable in route
+    method: 'GET',
+    credentials: 'include',  //sends user credentials, so it knows which user is sending request
+  })
     .then(function () {
       window.location.reload()   //anonymous function, not doing anything with response (vs code suggested this)
-      return
     })
-  }
-    //make an event listener for add email for push notifications
-    function updateEmail() {
-      //getting value of text field entered for email
-      const email = document.getElementById('updateEmail').value
-      fetch(`${window.location.protocol}//${window.location.host}/profile/email`, { //this constructs the url
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', //tells server this is a json request
-        },
-        credentials: 'include',  //sends user credentials, so it knows which user is sending request
-        body: JSON.stringify({email: email}) //takes JS object and turns it into a string
-      })
-      .catch((error)=> {  // in the case there was some error
-        console.error('Error:', error); // log an error to the console
-      });
-    }
+}
+//make an event listener for add email for push notifications
+function updateEmail() {
+  //getting value of text field entered for email
+  const email = document.getElementById('updateEmailId').value
+  fetch(`${window.location.protocol}//${window.location.host}/profile/email`, { //this constructs the url
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', //tells server this is a json request
+    },
+    credentials: 'include',  //sends user credentials, so it knows which user is sending request
+    body: JSON.stringify({ email: email }) //takes JS object and turns it into a string
+  }).then(
+    console.log("submitted email")
+  ).catch((error) => {  // in the case there was some error
+    console.error('Error:', error); // log an error to the console
+  });
+};
