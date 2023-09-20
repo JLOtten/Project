12a969261f1499aa80fdc3d7ad014f61621cd27e
jlotten <jenna.otten@gmail.com>
2023-09-19 20:32:47 -0700

    Cleaned up code, removed old comments and console.logs

--- static/coders_boost.js
+++ static/coders_boost.js
@@ -5,26 +5,26 @@ function copyText () {
   navigator.clipboard.writeText(`${window.location.protocol}//${window.location.host}?encouragement_id=${encouragementId}`)
 }
 
-// make an event listener for favorite button
+// an event listener for favorite button
 function favoriteEncouragement () {
   const element = document.getElementById('favoriteEncouragement')
 
-  const encouragementId = element.getAttribute('data-value') // Problem here, not getting the value of the button
+  const encouragementId = element.getAttribute('data-value') 
   console.log(`this is my encouragement I'm saving ${encouragementId}`)
 
-  fetch(`${window.location.protocol}//${window.location.host}/save-favorite`, { // this constructs the url
+  fetch(`${window.location.protocol}//${window.location.host}/save-favorite`, { 
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json' // tells server this is a json request
+      'Content-Type': 'application/json' 
     },
-    credentials: 'include', // sends user credentials, so it knows which user is sending request
-    body: JSON.stringify({ encouragement_id: encouragementId }) // takes JS object and turns it into a string
+    credentials: 'include', 
+    body: JSON.stringify({ encouragement_id: encouragementId }) 
   })
-    .then(function () { // anonymous function, not doing anything with response (vs code suggested this)
+    .then(function () { 
       console.log('success saving encouragement')
     })
-    .catch((error) => { // in the case there was some error
-      console.error('Error:', error) // log an error to the console
+    .catch((error) => { 
+      console.error('Error:', error) 
     })
 }
 
@@ -36,19 +36,19 @@ function deleteFavoriteEncouragement (encouragementId) {
       'Content-Type': 'application/json' // tells server this is a json request
     },
     credentials: 'include', // sends user credentials, so it knows which user is sending request
-    body: JSON.stringify({ encouragement_id: encouragementId }) // takes JS object and turns it into a string
+    body: JSON.stringify({ encouragement_id: encouragementId }) 
   })
-    .then(function () { // anonymous function, not doing anything with response (vs code suggested this)
-      const element = document.getElementById('encouragement-' + encouragementId) // grabs div id & encouragement_id
-      element.remove() // Removes deleted encouragement from page on successful submission
+    .then(function () {  
+      const element = document.getElementById('encouragement-' + encouragementId)
+      element.remove() 
     })
-    .catch((error) => { // in the case there was some error
-      console.error('Error:', error) // log an error to the console
+    .catch((error) => { 
+      console.error('Error:', error) 
     })
 }
 
 // makes an AJAX request for getting new encouragments
-function getNextEncouragement () {
+function getNextEncouragement() {
   let encouragementId = 0 // global variable to store encouragement id
   fetch(`${window.location.protocol}//${window.location.host}/next-encouragement`, {
     method: 'GET',
@@ -58,15 +58,14 @@ function getNextEncouragement () {
     .then((data) => {
       document.getElementById('encouragement-text').innerHTML = data.text
 
-      encouragementId = data.id // not updating encouragementId
+      encouragementId = data.id 
       console.log(data)
       setButtonValuesById(encouragementId)
-    }) // changed this here to make an AJAX request
-    .catch((error) => { // in the case there was some error
-      console.error('Error:', error) // log an error to the console
+    }) 
+    .catch((error) => { 
+      console.error('Error:', error) 
     })
   document.getElementById('share-buttons').style.visibility = 'visible'
-  console.log(encouragementId)
   setButtonValuesById(encouragementId)
 }
 
@@ -81,8 +80,7 @@ function setButtonValuesById (id) {
   if (favoriteEncouragementElem) {
     favoriteEncouragementElem.setAttribute('data-value', id)
   }
-  // document.getElementById("facebook")
-  // document.getElementById("email").value = id;
+
   document.getElementById('copyButton').setAttribute('data-value', id)
 }
 
@@ -117,20 +115,3 @@ function retryFetch (url, options, retries = 3) {
     })
 }
 
-// make an event listener for add email for push notifications
-function updateEmail () {
-  // getting value of text field entered for email
-  const email = document.getElementById('updateEmailId').value
-  fetch(`${window.location.protocol}//${window.location.host}/profile/email`, { // this constructs the url
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json' // tells server this is a json request
-    },
-    credentials: 'include', // sends user credentials, so it knows which user is sending request
-    body: JSON.stringify({ email }) // takes JS object and turns it into a string
-  }).then(
-    console.log('submitted email')
-  ).catch((error) => { // in the case there was some error
-    console.error('Error:', error) // log an error to the console
-  })
-}
