cf9764d16a833096ebdc2b8c5325862132f4b29e
jlotten <jenna.otten@gmail.com>
2023-10-16 15:48:36 -0700

    3-D carousel changes on homepage

--- static/coders_boost.js
+++ static/coders_boost.js
@@ -115,3 +115,15 @@ function retryFetch (url, options, retries = 3) {
     })
 }
 
+//gallery spin controls for homepage
+var angle = 0;
+function galleryspin(sign) {
+	spinner = document.querySelector("#spinner");
+	if (!sign) {
+		angle = angle + 45;
+	} else {
+		angle = angle - 45;
+	}
+	spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg);
+	transform: rotateY("+ angle +"deg);");
+}
